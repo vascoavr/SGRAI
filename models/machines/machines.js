69667195b8659d8bcba38f61f6f7fac9f4d9ea02
Jorge Campos <1150759@isep.ipp.pt>
2019-11-11 19:06:22 +0000

    First type of machine

---
+++ models/machines/machines.js
@@ -0,0 +1,10 @@
+export const machines = [
+    {
+        type: 'M1',
+        model: './models/machines/factory_robot_arm'
+    }/* ,
+    {
+        type: 'M2',
+        model: './models/machines/drill_press'
+    } */
+]
