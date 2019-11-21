f25013e70abc756d3c0c503d1b685fcc041a6146
Jorge Campos <1150759@isep.ipp.pt>
2019-11-21 19:21:14 +0000

    Swap machines New machines

--- models/machines/machines.js
+++ models/machines/machines.js
@@ -2,9 +2,40 @@ export const machines = [
     {
         type: 'M1',
         model: './models/machines/factory_robot_arm'
-    }/* ,
+    },
     {
         type: 'M2',
-        model: './models/machines/drill_press'
-    } */
+        model: './models/machines/factory_robot_arm_2'
+    },
+    {
+        type: 'M3',
+        model: './models/machines/factory_robot_arm_3'
+    }
 ]
+
+export function setMachineProperties(machine, x, z) {
+    switch (machine.machineType) {
+        case 'M1':
+            machine.scale.set(10, 15, 10);
+            machine.position.set(48 * x - 110, 0, 50 * z - 102);
+            machine.rotation.set(0, rad(-90), 0);
+
+            break;
+        case 'M2':
+            machine.scale.set(10, 15, 10);
+            machine.position.set(48 * x - 110, 0, 50 * z - 102);
+            machine.rotation.set(0, rad(-90), 0);
+
+            break;
+        case 'M3':
+            machine.scale.set(10, 15, 10);
+            machine.position.set(48 * x - 110, 0, 50 * z - 102);
+            machine.rotation.set(0, rad(-90), 0);
+
+            break;
+        default:
+            break;
+    }
+
+    return machine;
+}
